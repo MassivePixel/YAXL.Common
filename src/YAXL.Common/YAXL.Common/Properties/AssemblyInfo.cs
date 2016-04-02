695a11c8874f8cff3327502804b27bd91b44c3b5
Toni <tpetrina@tpetrina.com>
2016-04-02 09:52:50 +0200

    Added empty initial project

---
+++ src/YAXL.Common/YAXL.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("YAXL.Common")]
+[assembly: AssemblyDescription ("Common code for Xamarin projects")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Massive Pixel d.o.o.")]
+[assembly: AssemblyProduct ("YAXL - Common")]
+[assembly: AssemblyCopyright ("Massive Pixel 2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("en-US")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("0.1.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
